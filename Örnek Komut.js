499d153113cb9f85295225658c592f17a3f8a428
THESUSPECT34 <37048926+THESUSPECT34@users.noreply.github.com>
2020-02-17 19:49:28 +0300

    Add files via upload

---
+++ Örnek Komut.js
@@ -0,0 +1,16 @@
+exports.run = (client, message) => {
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['Komutun Kullanım Şekilleri'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'Komutun Adı',
+  description: 'Komutun Açıklaması',
+  usage: 'Kommutun Kullanım Örneği'
+};
